76bc2b4c1ef08c211c710be17078b1e0d43224b2
is-qian <tangruiqian17@gmail.com>
2024-04-29 01:10:12 +0800

    add reComputer-R100x

---
+++ stage4a/00-dtoverlays/00-run.sh
@@ -0,0 +1,80 @@
+#!/bin/bash -e
+set -x
+
+SEEED_DEV_NAME=reComputer-R100x
+GIT_MODULE='https://github.com/Seeed-Studio/seeed-linux-dtoverlays.git -b master --depth=1'
+
+if [ "X$GIT_MODULE" != "X" ]; then
+	MODULE_PATH=/seeed-linux-dtoverlays
+	${PROXYCHAINS} git clone ${GIT_MODULE} "${ROOTFS_DIR}${MODULE_PATH}"
+	ls ${ROOTFS_DIR}${MODULE_PATH}
+	# ${PROXYCHAINS} wget http://192.168.1.77/reTerminalDM/dt-blob-disp1-cam2.bin -O "${ROOTFS_DIR}/boot/dt-blob.bin"
+
+	on_chroot << EOF
+cd ${MODULE_PATH}
+dpkg -l | grep kernel
+./scripts/reTerminal.sh --device ${SEEED_DEV_NAME}
+EOF
+
+	rm -rfv "${ROOTFS_DIR}${MODULE_PATH}"
+fi
+
+# cat ${WORK_DIR}/config
+
+if [ -f "purges" ]; then
+	log "Begin ${SUB_STAGE_DIR}/purges"
+	PACKAGES="$(sed -f "${SCRIPT_DIR}/remove-comments.sed" < "purges")"
+	if [ -n "$PACKAGES" ]; then
+		on_chroot << EOF
+apt-get autoremove --purge -y $PACKAGES
+EOF
+		if [ "${USE_QCOW2}" = "1" ]; then
+			on_chroot << EOF
+apt-get clean
+EOF
+		fi
+	fi
+	log "End ${SUB_STAGE_DIR}/purges"
+fi
+
+if [ -f "python-packages" ]; then
+	log "Begin ${SUB_STAGE_DIR}/python-packages"
+	PACKAGES="$(sed -f "${SCRIPT_DIR}/remove-comments.sed" < "python-packages")"
+	if [ -n "$PACKAGES" ]; then
+		on_chroot << EOF
+set -x
+pip3 install $PACKAGES
+EOF
+	fi
+	log "End ${SUB_STAGE_DIR}/python-packages"
+fi
+
+if [ -f "remove" ]; then
+	log "Begin ${SUB_STAGE_DIR}/remove"
+	PACKAGES="$(sed -f "${SCRIPT_DIR}/remove-comments.sed" < "remove")"
+	on_chroot << EOF
+rm -rfv $PACKAGES
+EOF
+	log "End ${SUB_STAGE_DIR}/remove"
+fi
+
+
+if [ "${COPY_FILES}" = "1" ]; then
+	log "Begin copy files special for seeed"
+	chmod +x ./files/dsi_fix.sh
+	cp ./files/dsi_fix.sh ${ROOTFS_DIR}/var/
+	cp ./files/seeed_dsifix.service ${ROOTFS_DIR}/lib/systemd/system/
+	on_chroot << EOF
+systemctl daemon-reload
+systemctl enable seeed_dsifix.service
+EOF
+	log "End copy files special for seeed"
+fi
+
+if [ "${COPY_DOCKER_IMG}" = "1" ]; then
+	if [ -d $BASE_DIR/docker_images ]; then
+		cp -r $BASE_DIR/docker_images "${ROOTFS_DIR}/var/"
+	else
+		log "docker image files not exist,check your CI code!"
+	fi
+fi
