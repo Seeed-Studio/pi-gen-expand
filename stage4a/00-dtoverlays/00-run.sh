5248bd530853cd760d664533a480f8982bedf472
is-qian <tangruiqian17@gmail.com>
2024-05-22 15:21:51 +0800

    reTerminal-plus:Default landscape screen after startup

--- stage4a/00-dtoverlays/00-run.sh
+++ stage4a/00-dtoverlays/00-run.sh
@@ -64,10 +64,17 @@ fi
 
 if [ -d "files" ]; then
 	log "Begin copy files special for seeed"
+	if [ -f "./files/wayfire.ini" ]; then
+		mkdir -p ${ROOTFS_DIR}/home/${FIRST_USER_NAME}/.config
+		cp ./files/wayfire.ini ${ROOTFS_DIR}/home/${FIRST_USER_NAME}/.config
+	fi
 	chmod +x ./files/dsi_fix.sh
 	cp ./files/dsi_fix.sh ${ROOTFS_DIR}/var/
 	cp ./files/seeed_dsifix.service ${ROOTFS_DIR}/lib/systemd/system/
 	on_chroot << EOF
+if [ -f "/home/${FIRST_USER_NAME}/.config" ]; then
+	chown -vR ${FIRST_USER_NAME}:${FIRST_USER_NAME} /home/${FIRST_USER_NAME}/.config 
+fi
 systemctl daemon-reload
 systemctl enable seeed_dsifix.service
 EOF
