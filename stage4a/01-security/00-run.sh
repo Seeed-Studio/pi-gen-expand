f80d3ee6e610c5e7237709d05ea2689bf5b59748
Tang Ruiqian <ruiqian.tang@seeed.cc>
2025-11-01 18:55:10 +0800

    fix: Update comment for apt source change and add keyring handling

--- stage4a/01-security/00-run.sh
+++ stage4a/01-security/00-run.sh
@@ -8,7 +8,7 @@ if [ -d "files/pam.d" ]; then
 	log "End ${SUB_STAGE_DIR}/files/pam.d"
 fi
 
-# For security authentication, change http in apt source to https(Only sources in deb822 format are supported)
+# For security authentication, change http in apt source to https
 on_chroot << EOF
 set -x
 grep -R --line-number -E 'http://' /etc/apt/sources.list /etc/apt/sources.list.d/* 2>/dev/null || echo "have no http source"
@@ -16,5 +16,10 @@ sed -i 's|http://|https://|g' /etc/apt/sources.list
 for file in /etc/apt/sources.list.d/*.sources ; do
 	[ -f "\$file" ] || continue
 	sed -i 's|http://|https://|g' "\$file"
+done
+curl -fsSL https://archive.raspberrypi.com/debian/raspberrypi.gpg.key | sudo gpg --dearmor -o /usr/share/keyrings/rpi-archive-keyring.gpg
+for file in /etc/apt/sources.list.d/*.list ; do
+	[ -f "\$file" ] || continue
+	sed -i 's|http://|[signed-by=/usr/share/keyrings/rpi-archive-keyring.gpg] https://|g' "\$file"
 done
 EOF
