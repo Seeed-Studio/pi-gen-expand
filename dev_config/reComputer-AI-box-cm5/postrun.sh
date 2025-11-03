37b060f8671f3262257de626ea1133e4479a660d
qian <ruiqiantang17@gmail.com>
2025-11-04 01:14:30 +0800

    feat: update postrun.sh to clean up temporary files and caches.

--- dev_config/reComputer-AI-box-cm5/postrun.sh
+++ dev_config/reComputer-AI-box-cm5/postrun.sh
@@ -71,4 +71,14 @@ uname -a
 git clone https://github.com/hailo-ai/hailo-rpi5-examples.git
 cd hailo-rpi5-examples
 sed -i 's/device_arch=.*$/device_arch=HAILO8/g' setup_env.sh
+sed -i '/sudo apt install python3-gi python3-gi-cairo/ s/$/ -y/' install.sh
 ./install.sh || true
+
+free -h
+swapon --show
+df -h
+
+# clean temp files and caches
+apt-get -y autoremove --purge
+apt-get -y clean
+rm -rf /tmp/*
--- dev_config/reComputer-R22/postrun.sh
+++ dev_config/reComputer-R22/postrun.sh
@@ -76,3 +76,8 @@ sed -i '/sudo apt install python3-gi python3-gi-cairo/ s/$/ -y/' install.sh
 free -h
 swapon --show
 df -h
+
+# clean temp files and caches
+apt-get -y autoremove --purge
+apt-get -y clean
+rm -rf /tmp/*
